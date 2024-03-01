da109b01d9ae02713c2a81d3d9e5a082c10d6d82
wep21 <daisuke.nishimatsu1021@gmail.com>
2024-03-01 10:51:09 +0900

    fix cmake lint
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

--- cmake/Modules/Finduuid.cmake
+++ cmake/Modules/Finduuid.cmake
@@ -1,3 +1,17 @@
+# Copyright 2024 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Try to find uuid
 # Once done, this will define
 #
@@ -24,11 +38,11 @@ find_library(UUID_LIBRARY
   PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(uuid
+find_package_handle_standard_args(UUID
   REQUIRED_VARS UUID_LIBRARY UUID_INCLUDE_DIR
   VERSION_VAR UUID_VERSION)
 
-if (UUID_FOUND AND NOT TARGET UUID::UUID)
+if(UUID_FOUND AND NOT TARGET UUID::UUID)
   mark_as_advanced(UUID_FOUND UUID_INCLUDE_DIR UUID_LIBRARY)
   add_library(UUID::UUID UNKNOWN IMPORTED)
   set_target_properties(UUID::UUID PROPERTIES
